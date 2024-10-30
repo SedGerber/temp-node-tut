9ed99f8ca25c8de05e8bb2085074163aa57fad75
segerber <segerber@student.ethz.ch>
2024-10-30 18:30:55 +0100

    .

--- 13-npm.js
+++ 13-npm.js
@@ -13,11 +13,14 @@
 // npm init (step by step, press enter to skip)
 // npm init -y (everything default)
 
+// npm i nodemon -D (or --save-dev) (for development only)
+
 const _ = require('lodash');
 
 const items = [1, [2, [3, [4]]]];
 const newItems = _.flattenDeep(items);
 console.log(newItems);
+console.log('Hello people');
 
 
 
